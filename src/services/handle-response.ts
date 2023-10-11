48ee619f2ffc580b6317902fd6d67b4f90abea15
ArpitS Tatva <ravindra.methaniya@tatvasoft.com>
2023-10-11 17:37:24 +0530

    error handle

--- src/services/handle-response.ts
+++ src/services/handle-response.ts
@@ -2,13 +2,15 @@ import {Alert} from 'react-native';
 import axiosInstance from './axios';
 
 export const callApi = async (url: string, customparams: any) => {
-  const response = await axiosInstance.get(url, {
-    params: customparams,
-  });
+  try {
+    const response = await axiosInstance.get(url, {
+      params: customparams,
+    });
 
-  if (response.status === 200) {
-    return response.data;
-  } else {
-    Alert.alert(response?.data?.message);
+    if (response.status === 200) {
+      return response.data;
+    }
+  } catch (error: any) {
+    Alert.alert(error);
   }
 };
