6f351e080d9b7ab2500e56d68b24276684d26872
ArpitS Tatva <arpit.singh@tatvasoft.com>
2023-10-12 20:16:49 +0530

    counter using native module

--- src/screens/home/home.screen.tsx
+++ src/screens/home/home.screen.tsx
@@ -1,10 +1,42 @@
-import React from 'react';
-import {Text, SafeAreaView} from 'react-native';
+import React, {useEffect} from 'react';
+import {Text, SafeAreaView, Button, NativeEventEmitter} from 'react-native';
+import {NativeModules} from 'react-native';
+
+const CounterEvents = new NativeEventEmitter(NativeModules.Counter);
 
 export const HomeScreen = () => {
+  useEffect(() => {
+    CounterEvents.addListener('onIncrement', result => {
+      console.log('increament received==', result);
+    });
+
+    CounterEvents.addListener('onDecrement', result => {
+      console.log('onDecrement received==', result);
+    });
+
+    return () => CounterEvents?.removeAllListeners();
+  }, []);
+
+  const increament = async () => {
+    NativeModules.Counter.increment(result =>
+      console.log('increment==', result),
+    );
+  };
+
+  const decreament = async () => {
+    try {
+      const result = await NativeModules.Counter.decrement();
+      console.log('decreament==', result);
+    } catch (error) {
+      console.log('decreament error==', error);
+    }
+  };
+
   return (
     <SafeAreaView>
       <Text>Home Screen</Text>
+      <Button title="Increment" onPress={() => increament()} />
+      <Button title="Decreament" onPress={() => decreament()} />
     </SafeAreaView>
   );
 };
